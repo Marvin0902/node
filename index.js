af92ce9e2633dce64eb2f1650e0b4223a7236740
Marvin <smite1u2ace3@gmail.com>
2024-10-13 11:59:30 +0800

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
 
 // 簡單的 API 路由
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js API with MySQLLLLYYYY');
+  res.send('Hello from Node.js API with MySQLLLLYYYYXXX');
 });
 
 // 查詢資料庫的 API
