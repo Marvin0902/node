a31eb38af43820a478d5fe6f8a4cc5e081fe3083
Marvin <smite1u2ace3@gmail.com>
2024-10-13 11:23:21 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+// 設定 MySQL 連接
+const pool = mysql.createPool({
+  host: 'mysql',
+  user: 'root',
+  password: 'example',
+  database: 'testdb',
+});
+
+
+
+// 簡單的 API 路由
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js API with MySQL');
+});
+
+// 查詢資料庫的 API
+app.get('/users', (req, res) => {
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API running at http://localhost:${port}`);
+});
